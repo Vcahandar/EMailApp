bcc09b30ba44541ad1c701111533c6aa59728866
Vcahandar <jahandarmv@code.edu.az>
2024-04-28 18:44:32 +0400

    ProfileController,AccountController,SendMessage action done

--- src/EMailApp.UI/Controllers/EMailMessageController.cs
+++ src/EMailApp.UI/Controllers/EMailMessageController.cs
@@ -1,7 +1,9 @@
 ﻿using EMailApp.Business.Interfaces;
 using EMailApp.Core.Concrete;
+using EMailApp.DataAccess.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EMailApp.UI.Controllers
 {
@@ -22,5 +24,47 @@ namespace EMailApp.UI.Controllers
             var messageList = _messageService.GetListReceiverMessage(e);
             return View(messageList);
         }
+
+
+        public async Task<IActionResult> Sendbox(string p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            p = values.Email;
+            var messageList = _messageService.GetListSenderMessage(p);
+            return View(messageList);
+        }
+
+
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(Message p)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            string mail = values.Email;
+            string name = values.Name + " " + values.Surname;
+            p.Date = DateTime.Now;
+            p.SenderMail = mail;
+            p.SenderName = name;
+            p.Status = true;
+            p.IsDraft = false;
+            p.IsRead = false;
+            EMailDbContext c = new EMailDbContext();
+            var usernamesurname = c.Users.Where(x => x.Email == p.ReceiverMail).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            p.ReceiverName = usernamesurname;
+            _messageService.TInsert(p);
+            return RedirectToAction("Inbox");
+
+        }
+
+
+
+
     }
 }
