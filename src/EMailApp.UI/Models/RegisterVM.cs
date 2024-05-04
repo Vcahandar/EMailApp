2e6007dd29688e82b729db29e90832fe07647436
Vcahandar <jahandarmv@code.edu.az>
2024-05-04 16:39:46 +0400

    Done

--- src/EMailApp.UI/Models/RegisterVM.cs
+++ src/EMailApp.UI/Models/RegisterVM.cs
@@ -1,27 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace EMailApp.UI.Models
+public class RegisterVM
 {
-    public class RegisterVM
-    {
-        [Required(ErrorMessage = "Please enter your name")]
-        public string Name { get; set; }
+    [Required(ErrorMessage = "Please enter your name")]
+    public string Name { get; set; }
 
-        [Required(ErrorMessage = "Please enter your surname")]
-        public string Surname { get; set; }
+    [Required(ErrorMessage = "Please enter your surname")]
+    public string Surname { get; set; }
 
-        [Required(ErrorMessage = "E-mail is not valid")]
-        public string Email { get; set; }
+    [Required(ErrorMessage = "E-mail is not valid")]
+    public string Email { get; set; }
 
-        [Required(ErrorMessage = "Please enter your username")]
-        public string Username { get; set; }
+    [Required(ErrorMessage = "Please enter your username")]
+    public string Username { get; set; }
 
-        [Required(ErrorMessage = "Please enter your password")]
-        public string Password { get; set; }
+    [Required(ErrorMessage = "Please enter your password")]
+    public string Password { get; set; }
 
-        [Compare("Password", ErrorMessage = "Passwords are not compatible")]
-        public string ConfirmPassword { get; set; }
+    [Compare("Password", ErrorMessage = "Passwords are not compatible")]
+    public string ConfirmPassword { get; set; }
 
-        public string ImageUrl { get; set; }
-    }
+    // [Required(ErrorMessage = "The ImageUrl field is required.")]
+    public string? ImageUrl { get; set; }
 }
